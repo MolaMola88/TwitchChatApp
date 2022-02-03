e0b8395c43d8eab08e4286381ad241c01d89518f
MolaMola88 <MolaMola88@github.com>
2022-02-03 19:46:58 -0300

    Primeira versão do bot

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const tmi = require('tmi.js');
+
+const twitchUserName = process.env.TWITCH_BOT_USERNAME;
+const twitchToken = process.env.TWITCH_OAUTH_TOKEN;
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true
+    },
+	options: { debug: true },
+	identity: {
+		username: twitchUserName,
+		password: twitchToken
+	},
+	channels: [ 'PauloSGarciaJ' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message === 'Oi' || message === 'oi') {
+		// "@alca, heya!"
+	    client.say(channel, `@${tags.username}, Olá, seja bem vindo!`);
+	}
+
+    //Hnadler
+    if(self || !message.startsWith('!')) return;
+
+	const args = message.slice(1).split(' ');
+	const command = args.shift().toLowerCase();
+
+	//if(command === 'echo') {
+		//client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
+	//}
+});
