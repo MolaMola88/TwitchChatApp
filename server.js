192e5e107d3d9b9ab885efb1d3698dfa7687362f
MolaMola88 <MolaMola88@github.com>
2022-02-15 00:09:39 -0300

    port

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 5000, () => {  
+server.listen(process.env.PORT , () => {  
     console.log('listening on *:3000');
 })
