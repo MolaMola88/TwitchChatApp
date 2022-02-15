3a2a818f8103c2861ce79f7edcc936fe6585302d
MolaMola88 <MolaMola88@github.com>
2022-02-14 23:59:21 -0300

    8080

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(80, () => {  
-    console.log('listening on *:80');
+server.listen(8080, () => {  
+    console.log('listening on *:8080');
 })
