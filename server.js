3c7d7362d8fa5b7dd40f45b1e647cdc18e984d5f
MolaMola88 <MolaMola88@github.com>
2022-02-14 23:50:25 -0300

    Chat implementado

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const tmi = require('tmi.js');
 const twitchUserName = process.env.TWITCH_BOT_USERNAME;
 const twitchToken = process.env.TWITCH_OAUTH_TOKEN;
 
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const client = new tmi.Client({
     connection: {
         reconnect: true
@@ -19,25 +26,16 @@ const client = new tmi.Client({
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if(self) return;
-
-	if(message === 'Oi' || message === 'oi') {
-		// "@alca, heya!"
-	    client.say(channel, `@${tags.username}, Olá, seja bem vindo!`);
-	}
-    if(message === 'Hi' || message === 'Hello') {
-		// "@alca, heya!"
-	    client.say(channel, `@${tags.username}, Welcome!`);
-    }
-    //Hnadler
-    if(self || !message.startsWith('!')) return;
-
-	const args = message.slice(1).split(' ');
-	const command = args.shift().toLowerCase();
-
-	//if(command === 'echo') {
-		//client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
-	//}
+app.get('/', (req, res) => {  
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {  
+    client.on('message', (channel, tags, message, self) => {   
+        io.emit('chat message', message);  
+    });
+});
+
+server.listen(3000, () => {  
+    console.log('listening on *:3000');
+})
