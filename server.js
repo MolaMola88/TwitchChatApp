b32aa3f1c5f1a845adb5326594675e049bfc7aaa
MolaMola88 <MolaMola88@github.com>
2022-02-23 19:48:59 -0300

    Anim+limite de msgs

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const tmi = require('tmi.js');
 const twitchUserName = process.env.TWITCH_BOT_USERNAME;
 const twitchToken = process.env.TWITCH_OAUTH_TOKEN;
 
+//Criação do servidor no express
 const express = require('express');
 const app = express();
 const http = require('http');
+//O app meio que controla as funções etc dentro do server
 const server = http.createServer(app);
+
+//O servidor http é passado como parametro pro servidor io
 const { Server } = require("socket.io");
 const io = new Server(server);
 
@@ -26,6 +30,7 @@ const client = new tmi.Client({
 
 client.connect();
 
+//O app ao atingir a home do website é chamado res que envia pra página o arquivo html pro servidor
 app.get('/', (req, res) => {  
     res.sendFile(__dirname + '/index.html');
 });
@@ -36,6 +41,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT , () => {  
-    console.log('listening on *:3000');
+server.listen(process.env.PORT || 3000, () => {  
+    console.log('listening on *:PORT');
 })
