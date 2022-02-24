51729228bdd7d2e8c699a68c8c0ef349633181e2
Guilherme <guilherme.prokisch@gmail.com>
2022-02-23 21:40:16 -0300

    arrumado varias coisas

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const tmi = require('tmi.js');
 
 const twitchUserName = process.env.TWITCH_BOT_USERNAME;
 const twitchToken = process.env.TWITCH_OAUTH_TOKEN;
@@ -8,39 +7,53 @@ const twitchToken = process.env.TWITCH_OAUTH_TOKEN;
 //Criação do servidor no express
 const express = require('express');
 const app = express();
+
+// Faz um mapa para os arquivos estaticos /static olha dentro da pasta public
+app.use('/static', express.static('public'));
+
 const http = require('http');
 //O app meio que controla as funções etc dentro do server
 const server = http.createServer(app);
 
 //O servidor http é passado como parametro pro servidor io
 const { Server } = require("socket.io");
-const io = new Server(server);
+const socket = new Server(server);
 
-const client = new tmi.Client({
+const tmi = require('tmi.js');
+
+const twitch = new tmi.Client({
     connection: {
         reconnect: true
     },
-	options: { debug: true },
-	identity: {
-		username: twitchUserName,
-		password: twitchToken
-	},
-	channels: [ 'PauloSGarciaJ' ]
+    options: {
+        debug: true
+    },
+    identity: {
+        username: twitchUserName,
+        password: twitchToken
+    },
+    channels: ['PauloSGarciaJ']
 });
 
-client.connect();
+twitch.connect();
 
 //O app ao atingir a home do website é chamado res que envia pra página o arquivo html pro servidor
-app.get('/', (req, res) => {  
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {  
-    client.on('message', (channel, tags, message, self) => {   
-        io.emit('chat message', message);  
+socket.on('connection', (socket) => {
+    twitch.on('message', (channel, tags, message, self) => {
+        var payload = { "usuario": tags.username, "msg": message };
+        socket.emit('chat message', payload);
     });
 });
 
-server.listen(process.env.PORT || 3000, () => {  
+socket.on('disconnect', function () {
+    socket.removeAllListeners("chat message");
+})
+
+
+server.listen(process.env.PORT || 3000, () => {
     console.log('listening on *:PORT');
 })
